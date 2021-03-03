1dde8f4969467c5c6d4e177f893e17a989211769
Rostyslav Skrypnyk <rostyslav.skrypnyk@gmail.com>
2021-03-03 21:23:31 +0100

    debug commit to test Travis's env variables.

--- install_catch2.sh
+++ install_catch2.sh
@@ -11,6 +11,7 @@ set -ex # exit immediatly after command fails, add '+' in front of output.
 VERSION=2.13.4
 
 # Install Catch2 from sources
+echo test env var from travis: ${TRAVIS_OS_NAME}
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     wget https://github.com/catchorg/Catch2/archive/v${VERSION}.tar.gz
 elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
