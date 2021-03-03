67a22b01c39de0140ab32431392afe2dbc4de360
Rostyslav Skrypnyk <rostyslav.skrypnyk@gmail.com>
2021-03-03 21:20:01 +0100

    Corrected downloading links on different platforms and corrected loaded compiler.

--- install_catch2.sh
+++ install_catch2.sh
@@ -6,12 +6,17 @@
 # This installs Catch2 into the VM.
 #
 
-set -ex
+set -ex # exit immediatly after command fails, add '+' in front of output.
 
-VERSION=2.9.1
+VERSION=2.13.4
 
 # Install Catch2 from sources
-wget https://github.com/catchorg/Catch2/archive/v${VERSION}.tar.gz
+if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+    wget https://github.com/catchorg/Catch2/archive/v${VERSION}.tar.gz
+elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+    curl -L -O https://github.com/catchorg/Catch2/archive/v${VERSION}.tar.gz
+fi
+
 tar -xf v${VERSION}.tar.gz
 cd Catch2-${VERSION}/
 cmake -Bbuild -H. -DBUILD_TESTING=OFF
